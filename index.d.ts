1a7bf75ce808a2f80e4b01cf1211205dcabda4c3
tehlordvortex <hadokoda@yahoo.com>
2018-02-06 22:27:41 +0100

    add typescript definition file

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for ajaxscroll 1.0.0
+// Project: ajaxscroll
+// Definitions by: Victor Akinyemi <https://tehlordvortex.github.io>
+
+export as namespace AJAXScroll;
+
+export function attach(xmlhttp?: XMLHttpRequest): void;
+export function detach(xmlhttp?: XMLHttpRequest): void;
